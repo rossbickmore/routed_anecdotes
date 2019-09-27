efaa6a318623217310b31a4b27f5ecfa72c3f83f
Ross Bickmore <rossbickmore@gmail.com>
2019-09-27 16:27:14 +0100

    completed step 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Route, Link, Redirect, withRouter
+} from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -6,9 +10,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link to="/" style={padding}>anecdotes</Link>
+      <Link to="/create"  style={padding}>create new</Link>
+      <Link to="/about"  style={padding}>about</Link>
     </div>
   )
 }
@@ -124,12 +128,14 @@ const App = () => {
 
   return (
     <div>
+    <Router>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
+      <Route exact path="/" render={() => <AnecdoteList anecdotes={anecdotes} />} />
+      <Route exact path="/about" render={() => <About />} />
+      <Route exact path="/create" render={() => <CreateNew addNew={addNew} />} />
+    </Router>
+    <Footer />
     </div>
   )
 }
